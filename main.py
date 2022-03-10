35dc984f1970c00d48500b2a5b37d02cca3d8cea
matheusblm <matheus_bueno10@hotmail.com>
2022-03-10 17:05:41 -0300

    feat: adicionado dia 10

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from os import makedirs
 def criar_dias_mes(mes):
     makedirs(f"./{mes} ", exist_ok=True)
     file = "daily.md"
-    for x in range(9,28):
+    for x in range(10,31):
         path = f'{mes}/{x}'
         makedirs(path, exist_ok=True)
         with open(os.path.join(path, file), 'w') as fp:
             pass
-criar_dias_mes("Fevereiro")
+criar_dias_mes("Março")
